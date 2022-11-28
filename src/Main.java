b223bc334d48e5553c776b336af571cf97f550ba
aleksdzie <aleksandra.dziedzic1@gmail.com>
2022-11-28 18:29:23 +0100

    Created list

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> console1=new LinkedList<String>();
+        console1.add("xbox");
+        console1.add("xbox");
+        console1.add("ps5");
+        console1.add("ps2");
+        console1.add("xbox");
+        console1.add("ps4");
+        console1.add("nintendo");
+        console1.add("pegazus");
+        console1.add("ps2");
+        console1.add("ps1");
+        System.out.println(console1);
+    }
+}
