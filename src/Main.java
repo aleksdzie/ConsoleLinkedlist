b6e3b0a1b0cd3c90ed28a49cfef4224c0b2dd3cd
aleksdzie <aleksandra.dziedzic1@gmail.com>
2022-12-03 14:14:17 +0100

    Added for each loop

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,23 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<String> console1=new LinkedList<String>();
-        console1.add("xbox");
-        console1.add("xbox");
-        console1.add("ps5");
-        console1.add("ps2");
-        console1.add("xbox");
-        console1.add("ps4");
-        console1.add("nintendo");
-        console1.add("pegazus");
-        console1.add("ps2");
-        console1.add("ps1");
-        System.out.println(console1);
+        LinkedList<String> konsole = new LinkedList<String>();
+        konsole.add("xbox");
+        konsole.add("xbox");
+        konsole.add("ps5");
+        konsole.add("ps2");
+        konsole.add("xbox");
+        konsole.add("ps4");
+        konsole.add("nintendo");
+        konsole.add("pegazus");
+        konsole.add("ps2");
+        konsole.add("ps1");
+        int count = 0;
+        for (int i = 0; i < konsole.length; i++){
+            if (konsole[i] == "xbox") {
+                count++;
+            }
+        }
+        System.out.println("Znaleziono "+count+ " xboxów.");
     }
 }
